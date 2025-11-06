ae12dce1cb02be1b57302353f5b66ba119d438ae
Md Yamin <167641353+mdyamin99@users.noreply.github.com>
2025-11-06 12:46:05 +0600

    Time: 8 ms (62.45%), Space: 14.8 MB (57.66%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> mp;
+        for(auto c : s){
+            mp[c]++;
+        }
+        int ans = -1;
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]] == 1){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
